4bf883cd4f1061a59ff329b9d27f4854b80aef2c
Scott Griffis <birdoffire1549@gmail.com>
2025-07-04 00:22:55 -0500

    Fixes to v1 of the firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 
 /*
   Firmware: .... presence-aware-switch
-  Version: ..... 1.2.0
+  Version: ..... 1.3.0
   Hardware: .... ESP-32
   Author: ...... Scott Griffis
-  Date: ........ 06/15/2025
+  Date: ........ 07/04/2025
   
   Description:
     This firmware is intended for an Expressif ESP-32 device. It allows for the 
@@ -15,10 +15,18 @@
 */
 
 #include <Arduino.h>
-#include <ArduinoBLE.h>
 #include <Settings.h>
 #include <map>
 #include <vector>
+#include <WiFi.h>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLEEddystoneURL.h>
+#include <BLEEddystoneTLM.h>
+#include <BLEBeacon.h>
 
 #define PAIR_PIN 32
 #define LEARN_LED_PIN 13
@@ -40,20 +48,29 @@ void doCheckForCloseDevice();
 void doHandleButtonPresses();
 void doCheckFactoryReset();
 
-std::map<String, ulong> seenDevices;
-std::map<String, int> seenRssis;
-std::vector<String> purgeList;
+void handleBTScanResults(BLEScanResults);
 
+std::map<std::string, ulong> seenDevices;
+std::map<std::string, int> seenRssis;
+
+BLEScan *scan;
+
+// Action Trigger Flags
 bool triggerFactoryReset = false;
 bool triggerDeviceLearn = false;
 
+// State Flags
+bool isLearning = false;
+bool isScanning = false;
+
 /**
  * SETUP
- * =====
+ * =======================================
  * The main setup portion of the firmware.
  * 
  */
 void setup() {
+  WiFi.mode(WIFI_OFF);
   pinMode(PAIR_PIN, INPUT);
   pinMode(CONTROLLED_DEVICE_PIN, OUTPUT);
   pinMode(LEARN_LED_PIN, OUTPUT);
@@ -66,16 +83,18 @@ void setup() {
   digitalWrite(CLOSE_LED_PIN, LOW);
 
   // Initialize Serial for Output
-  Serial.begin(9600);
+  Serial.begin(115200);
   if (!Serial) ESP.restart();
 
-  // Initialize Bluetooth
-  Serial.print("Starting Bluetooth... ");
-  if (!BLE.begin()) {
-    Serial.println("Issue with starting Bluetooth!");
-    delay(5000L);
-    ESP.restart();
-  }
+  BLEDevice::init("");
+
+  Serial.print("Reinitializing Bluetooth... ");
+  scan = BLEDevice::getScan();
+  scan->setActiveScan(true);  //active scan uses more power, but get results faster
+  scan->setInterval(100);
+  scan->setWindow(99);  // less or equal setInterval value
+  scan->start(5, handleBTScanResults);
+  isScanning = true;
   Serial.println("Complete.");
 
   Serial.printf("Learn Hold: %d millis\n", settings.getEnableLearnHoldMillis());
@@ -83,13 +102,11 @@ void setup() {
   Serial.printf("Max Not Seen: %d millis\n", settings.getMaxNotSeenMillis());
   Serial.printf("Max Near RSSI: %d \n", settings.getMaxNearRssi());
   Serial.printf("Paired Address: %s\n", settings.getParedAddress().c_str());
-
-  BLE.scan();
 }
 
 /**
  * MAIN LOOP
- * =========
+ * ======================================
  * The main looping part of the firmware.
  * 
  */
@@ -108,6 +125,7 @@ void loop() {
  * This function is the sole handler of the learn button's 
  * functionality. It notifies other functions when various tasks
  * need to be performed using boolean event flags.
+ * 
  */
 void doHandleButtonPresses() {
   static ulong timerMillis = 0UL;
@@ -142,6 +160,7 @@ void doHandleButtonPresses() {
  * Checks to see if a device is close enough to be a good
  * pair candidate and if so turns on the close device indicator
  * LED. If not it ensures the LED is off.
+ * 
  */
 void doCheckForCloseDevice() {
   bool isClose = false;
@@ -188,10 +207,7 @@ void doCheckFactoryReset() {
  */
 void doDeterminePairedDeviceProximity() {
   bool sState = settings.isOnState();
-  settings.setOnState(seenDevices.count(settings.getParedAddress()) > 0);
-  if (sState != settings.isOnState()) {
-    settings.saveSettings();
-  }
+  settings.setOnState(seenDevices.count(settings.getParedAddress().c_str()) > 0);
 }
 
 /**
@@ -215,63 +231,51 @@ void doHandleOnOffSwitching() {
 }
 
 /**
- * Used to perge old seen devices which are no longer considered
+ * Used to purge expired seen devices which are no longer considered
  * to be in-range.
  * 
  */
 void doPurgeOldSeenDevices() {
-  // Purge seenDevices that are too old
+  // Purge seenDevices that are expired
   int devCount = seenDevices.size();
-  String deleteList[devCount] = {""};
+  if (devCount > 0) {
+    std::vector<std::string> purgeList;
 
-  // Locate old devices which need purged
-  for (const auto& pair : seenDevices) {
-    if (millis() - pair.second > settings.getMaxNotSeenMillis()) {
-      purgeList.push_back(pair.first);
+    // Locate expired devices which need purged
+    for (const auto& pair : seenDevices) {
+      if (millis() - pair.second > settings.getMaxNotSeenMillis()) {
+        purgeList.push_back(pair.first);
+      }
     }
-  }
 
-  // Purge the identified devices
-  for (String id : purgeList) {
-    seenDevices.erase(id);
-    seenRssis.erase(id);
-    if (
-      settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx") 
-      || settings.getParedAddress().equalsIgnoreCase(id)
-    ) { 
-      Serial.printf("Purged 'seen' device; device=[%s]\n", id.c_str());
+    // Purge the identified expired devices
+    for (std::string id : purgeList) {
+      seenDevices.erase(id);
+      seenRssis.erase(id);
+      if (
+        settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx") 
+        || settings.getParedAddress().equalsIgnoreCase(String(id.c_str()))
+      ) { 
+        Serial.printf("Purged 'seen' device; device=[%s]\n", id.c_str());
+      }
     }
+    purgeList.clear();
   }
-  purgeList.clear();
 }
 
 /**
- * Handles doing the Bluetooth Scan routine for identifying
- * nearby devices also initiates the purging of old devices
- * which had been seen last too long ago.
+ * Handles tasks related to BlueTooth scans by first
+ * purging stored devices not seen past their expiration
+ * time, then it kicks off the scan again if it has completed.
  * 
  */
 void doBTScan() {
   doPurgeOldSeenDevices();
 
-  // Add or update seen device to map
-  BLEDevice dev = BLE.available();
-  if (dev && dev.rssi() > settings.getMaxNearRssi()) {
-    // Seen device is not out of range
-    if (settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx")) {
-      Serial.printf("Near device; device=[%s]; rssid=[%d]\n", dev.address().c_str(), dev.rssi());
-    } else if (settings.getParedAddress().equalsIgnoreCase(dev.address())) {
-      Serial.printf("Device Checked In! DeviceID=[%s]; RSSI=[%d];\n", dev.address().c_str(), dev.rssi());
-    }
-    seenDevices[dev.address()] = millis();
-    seenRssis[dev.address()] = dev.rssi();
-  } else if (dev) {
-    if (
-      settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx") 
-      || settings.getParedAddress().equalsIgnoreCase(dev.address())
-    ) {
-      Serial.printf("Seen device RSSI too low! DeviceID=[%s]; RSSI=[%d];\n", dev.address().c_str(), dev.rssi());
-    }
+  if (!isScanning) {
+    // Start scanning when it is done
+    scan->start(5, handleBTScanResults);
+    isScanning = true;
   }
 }
 
@@ -283,32 +287,32 @@ void doBTScan() {
  */
 void doCheckLearnTask() {
   static ulong learnStartMillis = 0L;
-  static bool learnStarted = false;
-
+  
   if (triggerDeviceLearn) {
     // Do start of learning tasks
-    if (!learnStarted) {
+    if (!isLearning) {
       digitalWrite(LEARN_LED_PIN, HIGH);
       learnStartMillis = millis();
       Serial.printf("Learning started...\n");
-      learnStarted = true;
+      isLearning = true;
     }
 
     // Wait 10 Seconds to allow nearest discovery then pair with nearest
     if (millis() - learnStartMillis > settings.getLearnWaitMillis()) {
-      String nearestId = "";
+      std::string nearestId = "";
       int nearestRssi = -999;
 
       // Check known devices for nearest
       for (const auto& pair : seenDevices) {
-        if (nearestId.isEmpty() || seenRssis[pair.first] > nearestRssi) {
+        if (String(nearestId.c_str()).isEmpty() || seenRssis[pair.first] > nearestRssi) {
           nearestId = pair.first;
           nearestRssi = seenRssis[pair.first];
         }
       }
+
       // Pair with identified ID
-      if (!settings.getParedAddress().equalsIgnoreCase(nearestId)) {
-        settings.setParedAddress(nearestId);
+      if (!settings.getParedAddress().equalsIgnoreCase(String(nearestId.c_str()))) {
+        settings.setParedAddress(String(nearestId.c_str()));
         settings.saveSettings();
         Serial.printf("Learning Complete! Paired Device is '%s', with RSSI of: %d\n\n", nearestId.c_str(), nearestRssi);
       } else {
@@ -316,10 +320,56 @@ void doCheckLearnTask() {
       }
 
       // Do end of learning tasks
-      learnStarted = false;
+      isLearning = false;
       triggerDeviceLearn = false;
 
       digitalWrite(LEARN_LED_PIN, LOW);
     }
   }
+}
+
+/**
+ * This function is used to handle BlueTooth LE scan results.
+ * Essentially it logs the information about the devices that 
+ * were seen during the scan.
+ * 
+ * When not tracking a specific device or in learning mode, 
+ * any device with a rssi lower than the acceptable max is 
+ * ignored while those with acceptable rssi's are recorded.
+ * 
+ * When tracking a specific device all devices except that 
+ * device are ignored. 
+ */
+void handleBTScanResults(BLEScanResults results) {
+  for (int i = 0; i < results.getCount(); i++) {
+    // Iterate and handle scanned devices
+    BLEAdvertisedDevice device = results.getDevice(i);
+
+    String btAddress = String(device.getAddress().toString().c_str());
+    int rssi = device.getRSSI();
+    
+    if (rssi > settings.getMaxNearRssi()) {
+      // Saw a device that is in-range
+      if (isLearning || settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx")) {
+        // Record all seen in-range if learning or not paired
+        Serial.printf("Near device; device=[%s]; rssid=[%d]\n", btAddress.c_str(), rssi);
+        seenDevices[btAddress.c_str()] = millis();
+        seenRssis[btAddress.c_str()] = rssi;
+      } else if (settings.getParedAddress().equalsIgnoreCase(btAddress)) {
+        // Only record device being tracked
+        Serial.printf("Device Checked In! DeviceID=[%s]; RSSI=[%d];\n", btAddress.c_str(), rssi);
+        seenDevices[btAddress.c_str()] = millis();
+        seenRssis[btAddress.c_str()] = rssi;
+      }
+    } else {
+      // Seen device is out of range just log it
+      if (
+        settings.getParedAddress().equalsIgnoreCase("xx:xx:xx:xx:xx:xx") 
+        || settings.getParedAddress().equalsIgnoreCase(btAddress)
+      ) {
+        Serial.printf("Seen device RSSI too low! DeviceID=[%s]; RSSI=[%d];\n", btAddress.c_str(), rssi);
+      }
+    }
+  }
+  isScanning = false;
 }
