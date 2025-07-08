2256dccce1bb3604d3b2db047613a62b331aa6ff
Scott Griffis <birdoffire1549@gmail.com>
2025-07-07 23:49:00 -0500

    Some comments added

--- lib/LedMan/LedMan.cpp
+++ lib/LedMan/LedMan.cpp
@@ -1,13 +1,57 @@
+/*
+    LedMan.cpp
+    This is the code file for the LedMan Class.
+
+    The purpose of this class is to act as the sole master of device LEDs. All other parts of the
+    firmware must interact with the LEDs through this class. This allows for LEDs to have a kind
+    of overloaded functionality where they can be driven by different processes with some processes
+    having a higher priority than others. This way every process can use an LED when they wish without
+    having to worry about the current and final on/off state of the LED.
+
+    For example, say that process 'A' desires for an LED to remain lit, but process 'B' which has a 
+    higher priority wishes to flash the LED. Using this class, process 'B' can lock the LED for use
+    and flash it as dessired. When finished process 'B' can set the LED to off and release the lock.
+    At that point the state of process 'A' would take over and the LED would go back to remaining lit.
+
+    Written by: ... Scott Griffis
+    Date: ......... 07/07/2025
+*/
+
 #include <LedMan.h>
 
+/**
+ * Used to Register an LED with this class so that it can be 
+ * controlled by users of this class.
+ * 
+ * @param ledPin - This is the device pin for the LED as int.
+ */
 void LedMan::addLed(int ledPin, String ledId) {
-    regLeds[ledId.c_str()] = ledPin;
+    registeredLeds[ledId.c_str()] = ledPin;
 }
 
+/**
+ * Used to set the priority for a caller function/process.
+ * The lower the priority value the more priority the caller
+ * process has.
+ * 
+ * @param caller - The ID of the caller as String.
+ * @param priority - The priority of the caller as int.
+ */
 void LedMan::setCallerPriority(String caller, int priority) {
     priorities[caller.c_str()] = priority;
 }
 
+/**
+ * Used to obtain a lock for an LED. 
+ * This isn't an exclusive access kind of lock, it is more of a lock that 
+ * says both on and off states of the LED are important to this process/caller
+ * so as long as it has the priority to do so it may force the LED to be off 
+ * even if a lower priority processs/caller wants it on. Without a lock, when 
+ * a process sets the led to off another lower priority process can turn it on.
+ * 
+ * @param ledId - The String ID for the LED.
+ * @param caller - The String ID for the Caller.
+ */
 void LedMan::lockLed(String ledId, String caller) {
     if (locks.count(caller.c_str()) == 0 || locks[caller.c_str()].count(ledId.c_str()) == 0) {
         locks[caller.c_str()][ledId.c_str()] = 1;
@@ -53,7 +97,7 @@ void LedMan::ledOff(String ledId, String caller) {
 }
 
 void LedMan::loop() {
-    for (const auto& regLed : regLeds) {
+    for (const auto& regLed : registeredLeds) {
         // Determine current status for each LED
         int ledPin = regLed.second;
         std::string ledId = regLed.first;
--- lib/LedMan/LedMan.h
+++ lib/LedMan/LedMan.h
@@ -1,3 +1,21 @@
+/*
+    LedMan.h
+    This is the header file for the LedMan Class.
+
+    The purpose of this class is to act as the sole master of device LEDs. All other parts of the
+    firmware must interact with the LEDs through this class. This allows for LEDs to have a kind
+    of overloaded functionality where they can be driven by different processes with some processes
+    having a higher priority than others. This way every process can use an LED when they wish without
+    having to worry about the current and final on/off state of the LED.
+
+    For example, say that process 'A' desires for an LED to remain lit, but process 'B' which has a 
+    higher priority wishes to flash the LED. Using this class, process 'B' can lock the LED for use
+    and flash it as dessired. When finished process 'B' can set the LED to off and release the lock.
+    At that point the state of process 'A' would take over and the LED would go back to remaining lit.
+
+    Written by: ... Scott Griffis
+    Date: ......... 07/07/2025
+*/
 #ifndef LedMan_h
     #define LedMan_h    
     
@@ -19,10 +37,8 @@
         void loop();
 
     private:
-        std::map<std::string/*LedId*/, int/*PinNumber*/> regLeds;
-
+        std::map<std::string/*LedId*/, int/*PinNumber*/> registeredLeds;
         std::map<std::string/*Caller*/, int/*Priority*/> priorities;
-
         std::map<std::string/*Caller*/, std::map<std::string/*LedId*/, int/*Junk*/>> locks;
         std::map<std::string/*Caller*/, std::map<std::string/*LedId*/, int/*CallerState*/>> callerStates;
     };
